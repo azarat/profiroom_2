1a205e720bfddc2a0ddbcad4a96fd1de42e2cf14
Nevada51 <yura2.op@gmail.com>
2019-09-27 16:59:21 +0300

    catalog
    will be changed

---
+++ src/app/shared/interfaces/freelancerList.interface.ts
@@ -0,0 +1,9 @@
+// tslint:disable-next-line: class-name
+export interface freelancersListInterface {
+  avaible_gigs?: number;
+  descroption?: string;
+  comments_count?: number;
+  rating?: number;
+  min_price?: number;
+  liked?: boolean;
+}
