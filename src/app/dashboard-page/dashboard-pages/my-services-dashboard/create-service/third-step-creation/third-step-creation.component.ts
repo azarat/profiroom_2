32dec7bb7a86e510bf848584ff5565c9be2f446b
TheCubeStudio <TheCubeStudio>
2019-11-07 20:46:46 +0200

    third staps main part

--- src/app/dashboard-page/dashboard-pages/my-services-dashboard/create-service/third-step-creation/third-step-creation.component.ts
+++ src/app/dashboard-page/dashboard-pages/my-services-dashboard/create-service/third-step-creation/third-step-creation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-third-step-creation',
@@ -10,19 +10,52 @@ export class ThirdStepCreationComponent implements OnInit {
   packagesForm: FormGroup;
 
 
-
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+
+    this.packagesForm = this.fb.group({
+      name: null,
+      mainOptions: this.fb.array([
+      ])
+    });
+
   }
 
-  saveStep(){
+  // --------- get optionsArray -----------------
 
+  get mainOptionsArray() {
+    return this.packagesForm.get('mainOptions') as FormArray;
   }
+
+  // ----- add option to packages
+  addMainOptin(item) {
+    this.mainOptionsArray.push(this.fb.group(
+      {
+        title: null,
+        basic: false,
+        standart: false,
+        premium: false
+      }
+    ));
+  }
+
+  deleteMainOption(index) {
+    this.mainOptionsArray.removeAt(index);
+  }
+
+  saveStep() {
+    console.log(this.packagesForm.value);
+    // console.log(this.packagesForm.value.optionsArray = this.mainOptionsArray);
+  }
+
+
 
   changesArrayCounter() {
     const changes = new Array();
-    for(let i = 1; i <= 30; i++) {
+    for (let i = 1; i <= 30; i++) {
       changes.push(i);
     }
     return changes;
