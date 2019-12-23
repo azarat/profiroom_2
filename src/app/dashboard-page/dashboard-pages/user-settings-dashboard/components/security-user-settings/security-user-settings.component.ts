381a96e36c308686297b9ab0bd5a9ec5e05c4692
Nevada51 <yura2.op@gmail.com>
2019-12-23 18:09:13 +0200

    user-setting
    validation password

--- src/app/dashboard-page/dashboard-pages/user-settings-dashboard/components/security-user-settings/security-user-settings.component.ts
+++ src/app/dashboard-page/dashboard-pages/user-settings-dashboard/components/security-user-settings/security-user-settings.component.ts
@@ -1,27 +1,49 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { UserSettingsModel } from 'src/app/models/user-settings.model';
+import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
+
+    return (invalidCtrl || invalidParent);
+  }
+}
 
 @Component({
   selector: 'app-security-user-settings',
   templateUrl: './security-user-settings.component.html',
   styleUrls: ['./security-user-settings.component.scss']
 })
+
+
+
 export class SecurityUserSettingsComponent implements OnInit {
 
   @Input() userSettings: UserSettingsModel;
   public submited = false;
 
-  constructor() { }
+  myForm: FormGroup;
+
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      password: ['', [Validators.required]],
+      confirmPassword: ['']
+    }, { validator: this.checkPasswords });
+
+  }
 
   ngOnInit() {
   }
 
-  onSubmit(form) {
-    this.submited = true;
-    if (form.invalid) {
-      console.log('invalid');
-      return;
-    }
-    // this.addEducation();
+  checkPasswords(group: FormGroup) { // here we have the 'passwords' group
+    let pass = group.controls.password.value;
+    let confirmPass = group.controls.confirmPassword.value;
+
+    return pass === confirmPass ? null : { notSame: true }
   }
 }
