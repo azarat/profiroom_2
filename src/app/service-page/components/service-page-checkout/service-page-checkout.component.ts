46731e8002f1d1c09ff8121b96a2abb177d9257b
Nevada51 <yura2_op@ukr.net>
2020-03-11 18:59:14 +0200

    some comit

--- src/app/service-page/components/service-page-checkout/service-page-checkout.component.ts
+++ src/app/service-page/components/service-page-checkout/service-page-checkout.component.ts
@@ -11,27 +11,41 @@ import { ExtraFeaturesModel } from 'src/app/models/service-page/service-page.mod
 export class ServicePageCheckoutComponent implements OnInit {
   @Input() offerData: OfferDataInterface;
   @Input() chousenPackage;
+
+
   public extraFeatures: ExtraFeaturesModel;
 
-  public checkoutExtraFeatures: FormGroup;
+  // public checkoutExtraFeatures: FormGroup;
+  public checkoutExtraFeatures = {
+      extraTerms: '',
+      extraСhanges: '',
+      extraCommercial: '',
+  };
   constructor(
     private fb: FormBuilder
   ) {}
 
   ngOnInit() {
     console.log(this.chousenPackage);
+
     this.initForm();
   }
 
   initForm() {
-    this.checkoutExtraFeatures = this.fb.group({
-      extraTerms: [''],
-      extraСhanges: [''],
-      extraCommercial: [''],
+  //   this.checkoutExtraFeatures = this.fb.group({
+  //     extraTerms: [''],
+  //     extraСhanges: [''],
+  //     extraCommercial: [''],
+  //   });
+  //   this.offerData.extra_features.forEach((el: any) => {
+  //     this.checkoutExtraFeatures.addControl(el.title, this.fb.control(null));
+  //   });
+  // }
+
+  this.offerData.extra_features.forEach((el: any) => {
+      this.checkoutExtraFeatures[el.title] = null;
     });
-    this.offerData.extra_features.forEach((el: any) => {
-      this.checkoutExtraFeatures.addControl(el.title, this.fb.control(null));
-    });
+    // this.checkoutExtraFeatures
   }
 
 }
